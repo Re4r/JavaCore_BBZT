483a095b267b0139a95402983966f9a68046c657
KirillMar <re4r@bk.ru>
2023-04-17 20:15:35 +0300

    lesson1

--- src/lesson1/Exec1.java
+++ src/lesson1/Exec1.java
@@ -5,13 +5,41 @@ import java.util.List;
 
 public class Exec1 {
     public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>();
+
+        Employee employee1 = new Employee(100, "Fasert", "Looper", 400);
+        Employee employee2 = new Employee(200, "Utrep", "Lirety", 300);
+        Employee employee3 = new Employee(700, "Treyu", "Gertuo", 700);
+
+        employees.add(employee1);
+        employees.add(employee2);
+        employees.add(employee3);
+
+
 
     }
 }
 
 class Employee {
-    int id;
-    String name;
-    String surname;
-    int salary;
+    private int id;
+    private String name;
+    private String surname;
+    private int salary;
+
+    public Employee(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
