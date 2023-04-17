4dc2b9d864f22bb0ccabceca096060d047d2cf5a
KirillMar <re4r@bk.ru>
2023-04-17 20:05:04 +0300

    lesson1

---
+++ src/lesson1/Exec1.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exec1 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Employee {
+    int id;
+    String name;
+    String surname;
+    int salary;
+}
