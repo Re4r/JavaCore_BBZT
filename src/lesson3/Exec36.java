2e3f18b6560c10b8b057e545bb9ee5e044e26220
KirillMar <re4r@bk.ru>
2023-04-18 15:50:25 +0300

    lesson1

--- src/lesson3/Exec36.java
+++ src/lesson3/Exec36.java
@@ -7,6 +7,19 @@ public class Exec36 {
     public static void main(String[] args) {
         List<Double> doubles = new ArrayList<>();
         List<String> strings = new ArrayList<>();
+        List<Double> doubles1 = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>();
+
+        doubles1.add(1.34);
+        doubles1.add(4.97);
+        doubles1.add(89.94);
+
+        integers.add(4);
+        integers.add(7);
+        integers.add(79);
+
+        System.out.println(summator(doubles1));
+        System.out.println(summator(integers));
 
         doubles.add(1.1);
         doubles.add(2.2);
@@ -19,5 +32,13 @@ public class Exec36 {
 
     public static void show(List<?> list) {
         System.out.println(list);
+    }
+
+    public static double summator(List<? extends Number> numbers) {
+        double result = 0;
+        for (Number n : numbers) {
+            result += n.doubleValue();
+        }
+        return result;
     }
 }
