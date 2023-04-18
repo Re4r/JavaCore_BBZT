d01d42cb8ee5aa34d57a2075230f88d25ae426da
KirillMar <re4r@bk.ru>
2023-04-18 14:03:07 +0300

    lesson1

--- src/lesson3/Exec32.java
+++ src/lesson3/Exec32.java
@@ -4,9 +4,13 @@ public class Exec32 {
     public static void main(String[] args) {
 
         Info<String> info = new Info<>("Hello");
+        String s = info.getValue();
         System.out.println(info);
+        System.out.println(s);
         Info<Integer> info1 = new Info<>(444);
+        Integer i = info1.getValue();
         System.out.println(info1);
+        System.out.println(i);
 
     }
 }
@@ -18,8 +22,38 @@ class Info<T> {
         this.value = value;
     }
 
+    public T getValue() {
+        return value;
+    }
+
     @Override
     public String toString() {
         return "Info: " + "value = " + value;
+    }
+}
+
+class Pair<V1, V2> {
+    private V1 v1;
+    private V2 v2;
+
+    public Pair(V1 v1, V2 v2) {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public V1 getV1() {
+        return v1;
+    }
+
+    public V2 getV2() {
+        return v2;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "v1 = " + v1 +
+                ", v2 = " + v2 +
+                '}';
     }
 }
