7c3f35621a37aaf466bda1168887fa7d81822cc1
KirillMar <re4r@bk.ru>
2023-04-18 13:27:24 +0300

    lesson1

--- src/lesson3/Exec31.java
+++ src/lesson3/Exec31.java
@@ -1,7 +1,23 @@
 package lesson3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exec31 {
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+
+        list.add("111");
+        list.add("333");
+        list.add("444");
+
+        int sum = 0;
+        for (String s : list) {
+            sum += Integer.parseInt(s);
+        }
+
+        System.out.println(sum);
 
     }
 }
