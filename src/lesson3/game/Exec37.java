fcc744ae52150defe94c6cbfb648be480ea36c38
KirillMar <re4r@bk.ru>
2023-04-19 13:33:14 +0300

    game

--- src/lesson3/game/Exec37.java
+++ src/lesson3/game/Exec37.java
@@ -3,5 +3,14 @@ package lesson3.game;
 public class Exec37 {
     public static void main(String[] args) {
 
+        Scholar scholar1 = new Scholar("Qawer", 12);
+        Scholar scholar2 = new Scholar("Yrtew", 13);
+
+        Student student1 = new Student("Ubrter", 22);
+        Student student2 = new Student("Opqazsd", 19);
+
+        Employee employee1 = new Employee("Meryb", 35);
+        Employee employee2 = new Employee("Hertop", 28);
+
     }
 }
