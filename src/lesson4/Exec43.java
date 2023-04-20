42d77bc1e66ae1153a64fdfdb4f6d888da96f652
KirillMar <re4r@bk.ru>
2023-04-20 15:48:47 +0300

    collection

---
+++ src/lesson4/Exec43.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+import java.util.ArrayList;
+
+public class Exec43 {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            integers.add(i);
+        }
+        System.out.println(integers);
+    }
+}
