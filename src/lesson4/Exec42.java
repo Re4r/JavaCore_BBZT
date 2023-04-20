385963fe97d6d844d3d1b089220d2ae85c67d9c7
KirillMar <re4r@bk.ru>
2023-04-20 15:46:25 +0300

    collection

--- src/lesson4/Exec42.java
+++ src/lesson4/Exec42.java
@@ -1,16 +1,23 @@
 package lesson4;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Exec42 {
     public static void main(String[] args) {
 
-        ArrayList<Integer> integers = new ArrayList<>();
+        List<Integer> integers = new ArrayList<>(20);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             integers.add(i);
         }
 
+        List<Integer> integers1 = new ArrayList<>(integers);
+
         System.out.println(integers);
+        System.out.println(integers1);
+        System.out.println(integers.size());
+        System.out.println(integers1.size());
+        System.out.println(integers.equals(integers1));
     }
 }
