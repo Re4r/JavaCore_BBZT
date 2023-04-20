b49b290f035c9ec9a4d3b1bd1c7e36b900b0cdad
KirillMar <re4r@bk.ru>
2023-04-20 20:45:50 +0300

    collection

--- src/lesson4/Exec47.java
+++ src/lesson4/Exec47.java
@@ -1,5 +1,8 @@
 package lesson4;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Exec47 {
     public static void main(String[] args) {
 
@@ -8,8 +11,34 @@ public class Exec47 {
         Citizen citizen3 = new Citizen("CCC", 16);
         Citizen citizen4 = new Citizen("DDD", 27);
         Citizen citizen5 = new Citizen("EEE", 58);
+        Citizen citizen6 = new Citizen("FFF", 43);
+        Citizen citizen7 = new Citizen("GGG", 37);
+
+        List<Citizen> citizenList = List.of(citizen1, citizen2, citizen3, citizen4, citizen5);
+
+        LinkedList<Citizen> citizenLinkedList = new LinkedList<>();
+
+        citizenLinkedList.addAll(citizenList);
+
+        showCitizens(citizenLinkedList);
+        System.out.println("------------------------------------");
+
+        System.out.println(citizenLinkedList.get(2));
+        System.out.println("------------------------------------");
+
+        citizenLinkedList.add(citizen6);
+        citizenLinkedList.add(citizen7);
+
+        showCitizens(citizenLinkedList);
+        System.out.println("------------------------------------");
 
 
+
+    }
+    static void showCitizens(List<Citizen> citizens) {
+        for (Citizen c : citizens) {
+            System.out.println(c);
+        }
     }
 }
 
@@ -24,7 +53,7 @@ class Citizen {
 
     @Override
     public String toString() {
-        return "Student2 - " +
+        return "Citizen - " +
                 "name: '" + name + '\'' +
                 ", course: " + course;
     }
