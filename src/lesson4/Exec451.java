6dae4b0bd910e582d8679d33f51c5ab7bcb8b357
KirillMar <re4r@bk.ru>
2023-04-20 18:32:37 +0300

    collection

---
+++ src/lesson4/Exec451.java
@@ -0,0 +1,36 @@
+package lesson4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exec451 {
+    public static void main(String[] args) {
+
+        StringBuilder builder1 = new StringBuilder("AAA");
+        StringBuilder builder2 = new StringBuilder("BBB");
+        StringBuilder builder3 = new StringBuilder("CCC");
+        StringBuilder builder4 = new StringBuilder("DDD");
+        StringBuilder builder5 = new StringBuilder("EEE");
+
+        List<StringBuilder> stringBuilderList = new ArrayList<>();
+
+        stringBuilderList.add(builder1);
+        stringBuilderList.add(builder2);
+        stringBuilderList.add(builder3);
+        stringBuilderList.add(builder4);
+        stringBuilderList.add(builder5);
+
+        List<StringBuilder> stringBuilderList1 = new ArrayList<>(stringBuilderList);
+
+        System.out.println(stringBuilderList);
+        System.out.println(stringBuilderList1);
+
+        System.out.println(stringBuilderList.equals(stringBuilderList1));
+
+        stringBuilderList.set(0, new StringBuilder("JJJ"));
+        System.out.println(stringBuilderList);
+        System.out.println(stringBuilderList.equals(stringBuilderList1));
+        stringBuilderList.removeAll(stringBuilderList1);
+        System.out.println(stringBuilderList);
+    }
+}
