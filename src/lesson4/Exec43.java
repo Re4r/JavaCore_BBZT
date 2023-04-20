005efafa045695931e3ac18b8de67573f66f1fe2
KirillMar <re4r@bk.ru>
2023-04-20 15:56:44 +0300

    collection

--- src/lesson4/Exec43.java
+++ src/lesson4/Exec43.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 
 public class Exec43 {
     public static void main(String[] args) {
+
         ArrayList<Integer> integers = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            integers.add(i);
-        }
+        ArrayList<Integer> integers1 = new ArrayList<>();
+        for (int i = 0; i < 20; i++) integers.add(i);
         System.out.println(integers);
+        for (int i = 0; i < integers.size(); i++) integers1.add(i, integers.get(i));
+        System.out.println(integers1);
+
     }
 }
