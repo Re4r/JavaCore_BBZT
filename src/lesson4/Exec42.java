c617fc4e1e0d6178b69c3e116cb8ba5d71580532
KirillMar <re4r@bk.ru>
2023-04-20 14:50:49 +0300

    generix

---
+++ src/lesson4/Exec42.java
@@ -0,0 +1,16 @@
+package lesson4;
+
+import java.util.ArrayList;
+
+public class Exec42 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            integers.add(i);
+        }
+
+        System.out.println(integers);
+    }
+}
