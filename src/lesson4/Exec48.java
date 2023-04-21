0d5620131b8579d51e148a7ce68292adf00ba719
KirillMar <re4r@bk.ru>
2023-04-21 12:32:51 +0300

    collection

--- src/lesson4/Exec48.java
+++ src/lesson4/Exec48.java
@@ -1,4 +1,35 @@
 package lesson4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Exec48 {
+    public static void main(String[] args) {
+        String s = "madam";
+        List<Character> characterList = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            characterList.add(c);
+        }
+        System.out.println(characterList);
+
+        ListIterator<Character> forwardIterator = characterList.listIterator();
+        ListIterator<Character> reverseIterator = characterList.listIterator(characterList.size());
+
+        boolean isPalindrome = true;
+
+        while (forwardIterator.hasNext() && reverseIterator.hasPrevious()) {
+            if (forwardIterator.next() != reverseIterator.previous()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(s + " - Palindrome");
+        } else {
+            System.out.println(s + " - Not Palindrome");
+        }
+    }
 }
