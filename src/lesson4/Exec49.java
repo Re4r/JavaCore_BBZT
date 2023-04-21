2d7ba1bdc6235218676597bbb1f22bf840ad95bd
KirillMar <re4r@bk.ru>
2023-04-21 13:24:34 +0300

    collection

--- src/lesson4/Exec49.java
+++ src/lesson4/Exec49.java
@@ -2,31 +2,52 @@ package lesson4;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Exec49 {
     public static void main(String[] args) {
 
-        ArrayList<Integer> integers = new ArrayList<>();
+//        ArrayList<Integer> integers = new ArrayList<>();
+//
+//        Random random = new Random();
+//
+//        for (int i = 0; i < 30; i++) {
+//            integers.add(random.nextInt(100));
+//        }
+//
+//        System.out.println(integers);
+//        System.out.println("---------------------");
+//
+//        Collections.sort(integers);
+//
+//        System.out.println(integers);
+//        System.out.println("---------------------");
+//
+//        int index = Collections.binarySearch(integers, 33);
+//
+//        System.out.println(index);
+//        System.out.println("---------------------");
 
-        Random random = new Random();
+        List<Employee> employeeList = new ArrayList<>();
 
-        for (int i = 0; i < 30; i++) {
-            integers.add(random.nextInt(100));
+        Random random1 = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            employeeList.add(new Employee(random1.nextInt(100)
+                    , Integer.toString(random1.nextInt(100000))
+                    , random1.nextInt(10000)));
         }
 
-        System.out.println(integers);
-        System.out.println("---------------------");
+        show(employeeList);
 
-        Collections.sort(integers);
 
-        System.out.println(integers);
-        System.out.println("---------------------");
+    }
 
-        int index = Collections.binarySearch(integers, 33);
-
-        System.out.println(index);
-        System.out.println("---------------------");
+    public static void show(List<Employee> list) {
+        for (Employee e : list) {
+            System.out.println(e);
+        }
     }
 }
 
@@ -43,7 +64,7 @@ class Employee {
 
     @Override
     public String toString() {
-        return "Employee >" +
+        return "Employee > " +
                 "id: " + id +
                 ", name: '" + name + '\'' +
                 ", salary: " + salary;
