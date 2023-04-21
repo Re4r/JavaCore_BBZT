eacde66fa084aff1213c4fe8e53c7b7555baa811
KirillMar <re4r@bk.ru>
2023-04-21 13:11:28 +0300

    collection

--- src/lesson4/Exec49.java
+++ src/lesson4/Exec49.java
@@ -1,6 +1,7 @@
 package lesson4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Exec49 {
@@ -15,5 +16,36 @@ public class Exec49 {
         }
 
         System.out.println(integers);
+        System.out.println("---------------------");
+
+        Collections.sort(integers);
+
+        System.out.println(integers);
+        System.out.println("---------------------");
+
+        int index = Collections.binarySearch(integers, 33);
+
+        System.out.println(index);
+        System.out.println("---------------------");
+    }
+}
+
+class Employee {
+    private int id;
+    private String name;
+    private int salary;
+
+    public Employee(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee >" +
+                "id: " + id +
+                ", name: '" + name + '\'' +
+                ", salary: " + salary;
     }
 }
