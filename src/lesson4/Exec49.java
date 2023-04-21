11d6dadbcef0e0254d57f45d9ff64125af9a461a
KirillMar <re4r@bk.ru>
2023-04-21 13:01:10 +0300

    collection

--- src/lesson4/Exec49.java
+++ src/lesson4/Exec49.java
@@ -1,4 +1,19 @@
 package lesson4;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Exec49 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        Random random = new Random();
+
+        for (int i = 0; i < 30; i++) {
+            integers.add(random.nextInt(100));
+        }
+
+        System.out.println(integers);
+    }
 }
