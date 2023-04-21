dfae2f4406b75ffd2b9f99ddcf53f12f472f100c
KirillMar <re4r@bk.ru>
2023-04-21 19:55:28 +0300

    map

--- src/map/Exec414.java
+++ src/map/Exec414.java
@@ -5,3 +5,56 @@ public class Exec414 {
 
     }
 }
+
+class Cobalt {
+    private int id;
+    private int source;
+    private double code;
+
+    public Cobalt(int id, int source, double code) {
+        this.id = id;
+        this.source = source;
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public double getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return "Cobalt - " +
+                "id: " + id +
+                ", source: " + source +
+                ", code: " + code;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cobalt cobalt)) return false;
+
+        if (getId() != cobalt.getId()) return false;
+        if (getSource() != cobalt.getSource()) return false;
+        return Double.compare(cobalt.getCode(), getCode()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = getId();
+        result = 31 * result + getSource();
+        temp = Double.doubleToLongBits(getCode());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
