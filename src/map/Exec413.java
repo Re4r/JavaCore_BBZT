1ccfc52e1e85f1fa24fd06e4d8ce62203f6675ba
KirillMar <re4r@bk.ru>
2023-04-21 18:54:04 +0300

    map

--- src/map/Exec413.java
+++ src/map/Exec413.java
@@ -1,7 +1,21 @@
 package map;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Exec413 {
     public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            map.put(new Random().nextInt(100), Double.toString(new Random().nextDouble(100)));
+        }
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
 
     }
 }
