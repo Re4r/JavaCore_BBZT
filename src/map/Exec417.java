122f8ee24b42ff199c2be04f017926b928849a46
KirillMar <re4r@bk.ru>
2023-04-23 19:40:34 +0300

    map

--- src/map/Exec417.java
+++ src/map/Exec417.java
@@ -1,7 +1,21 @@
 package map;
 
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
 public class Exec417 {
     public static void main(String[] args) {
+
+        TreeMap<Integer, Double> treeMap = new TreeMap<>();
+
+        for (int i = 0; i < 10; i++) {
+            treeMap.put(new Random().nextInt(1000), new Random().nextDouble(1000));
+        }
+
+        for (Map.Entry<Integer, Double> entry : treeMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
 
     }
 }
