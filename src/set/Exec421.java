0d448f6770f5a905e41aeab688a0e613cd670bf1
KirillMar <re4r@bk.ru>
2023-04-25 01:01:08 +0300

    set

--- src/set/Exec421.java
+++ src/set/Exec421.java
@@ -1,6 +1,8 @@
 package set;
 
+import java.util.Comparator;
 import java.util.Random;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Exec421 {
@@ -16,7 +18,57 @@ public class Exec421 {
         System.out.println(integerTreeSet);
         System.out.println("----------------------------------------------------------------------------------------");
 
+        Set<Unreal> unrealSet = new TreeSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            unrealSet.add(new Unreal(new Random().nextInt(100), new Random().nextDouble(100)));
+        }
+
+        Comparator<Unreal> idComparator = new Comparator<Unreal>() {
+            @Override
+            public int compare(Unreal o1, Unreal o2) {
+                return Integer.compare(o1.getId(), o2.getId());
+            }
+        };
+
+        Comparator<Unreal> codeComparator = new Comparator<Unreal>() {
+            @Override
+            public int compare(Unreal o1, Unreal o2) {
+                return Double.compare(o1.getCode(), o2.getCode());
+            }
+        };
+
+        unrealSet.forEach(System.out::println);
+
 
 
     }
+}
+
+class Unreal {
+    private int id;
+    private double code;
+
+    public Unreal(int id, double code) {
+        this.id = id;
+        this.code = code;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return "Unreal{" +
+                "id=" + id +
+                ", code=" + code +
+                '}';
+    }
+
+
 }
