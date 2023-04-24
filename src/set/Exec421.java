3116e1c3d0afdc92a9d48dc971a01f192e091ec8
KirillMar <re4r@bk.ru>
2023-04-25 00:45:43 +0300

    set

--- src/set/Exec421.java
+++ src/set/Exec421.java
@@ -1,4 +1,22 @@
 package set;
 
+import java.util.Random;
+import java.util.TreeSet;
+
 public class Exec421 {
+    public static void main(String[] args) {
+
+        TreeSet<Integer> integerTreeSet = new TreeSet<>();
+
+        for (int i = 0; i < 30; i++) {
+            integerTreeSet.add(new Random().nextInt(30));
+        }
+
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println(integerTreeSet);
+        System.out.println("----------------------------------------------------------------------------------------");
+
+
+
+    }
 }
