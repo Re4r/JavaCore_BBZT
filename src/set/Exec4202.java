4a0a9b32607b7a4af237a9884d60e427131d54ad
KirillMar <re4r@bk.ru>
2023-04-25 00:39:35 +0300

    set

--- src/set/Exec4202.java
+++ src/set/Exec4202.java
@@ -2,6 +2,7 @@ package set;
 
 import java.util.HashSet;
 import java.util.Random;
+import java.util.function.Predicate;
 
 public class Exec4202 {
     public static void main(String[] args) {
@@ -11,8 +12,8 @@ public class Exec4202 {
 
 
         for (int i = 0; i < 20; i++) {
-            integerHashSet1.add(new Random().nextInt(100));
-            integerHashSet2.add(new Random().nextInt(100));
+            integerHashSet1.add(new Random().nextInt(10));
+            integerHashSet2.add(new Random().nextInt(10));
         }
 
         System.out.println("----------------------------------------------------------------------------------------");
@@ -25,6 +26,26 @@ public class Exec4202 {
         union.addAll(integerHashSet2);
         System.out.println(union);
         System.out.println("----------------------------------------------------------------------------------------");
+
+        HashSet<Integer> intersect = HashSet.newHashSet(10);
+        intersect.addAll(integerHashSet1);
+        intersect.retainAll(integerHashSet2);
+        System.out.println(intersect);
+        System.out.println("----------------------------------------------------------------------------------------");
+
+        HashSet<Integer> subtract = new HashSet<>(integerHashSet2);
+        subtract.removeAll(integerHashSet1);
+        System.out.println(subtract);
+        System.out.println("----------------------------------------------------------------------------------------");
+
+        Predicate<Integer> predicate = (x) -> x > 5 && x < 9;
+
+        HashSet<Integer> hashSet = new HashSet<>(integerHashSet1);
+        hashSet.removeIf(predicate);
+        System.out.println(hashSet);
+        System.out.println("----------------------------------------------------------------------------------------");
+
+
 
 
     }
