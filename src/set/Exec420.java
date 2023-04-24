544864516370d3b823a4027dbd8726d83aafc10d
KirillMar <re4r@bk.ru>
2023-04-24 12:44:01 +0300

    set

--- src/set/Exec420.java
+++ src/set/Exec420.java
@@ -1,21 +1,38 @@
 package set;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
 
 public class Exec420 {
     public static void main(String[] args) {
 
-        Set<Integer> doubleSet = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < 100; i++) {
-            doubleSet.add(new Random().nextInt(100));
+            set.add(new Random().nextInt(100));
         }
+
         int counter = 0;
-        for (var o : doubleSet) {
+        for (var o : set) {
             counter++;
             System.out.println(counter + " : " + o);
         }
+        System.out.println("----------------------------");
+
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            if ((Integer) iterator.next() % 2 != 0) {
+                iterator.remove();
+            }
+        }
+
+        int counter1 = 0;
+        for (var o : set) {
+            counter1++;
+            System.out.println(counter1 + " : " + o);
+        }
+        System.out.println("----------------------------");
     }
 }
