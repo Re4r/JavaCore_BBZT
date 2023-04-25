c596dc518dfc93c638a4dc559ffbe4809856c4a5
KirillMar <re4r@bk.ru>
2023-04-25 15:31:37 +0300

    set

--- src/queue/Exec425.java
+++ src/queue/Exec425.java
@@ -1,7 +1,25 @@
 package queue;
 
+import java.util.ArrayDeque;
+
 public class Exec425 {
     public static void main(String[] args) {
+
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        arrayDeque.addFirst(10);
+        arrayDeque.addLast(20);
+        arrayDeque.offer(11);
+        arrayDeque.offer(78);
+        arrayDeque.addLast(45);
+        arrayDeque.offer(99);
+
+        System.out.println(arrayDeque);
+
+        arrayDeque.removeFirst();
+        arrayDeque.removeLast();
+
+        System.out.println(arrayDeque);
 
     }
 }
