56eb3334730dd10d7289b273713312b2db91922c
KirillMar <re4r@bk.ru>
2023-04-25 14:14:05 +0300

    set

--- src/queue/Exec424.java
+++ src/queue/Exec424.java
@@ -8,20 +8,19 @@ import java.util.Random;
 public class Exec424 {
     public static void main(String[] args) {
 
-        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
-
-        for (int i = 0; i < 10; i++) {
-            queue1.add(new Random().nextInt(100));
-        }
-
-        System.out.println(queue1);
-        System.out.println("-------------------------------");
-
+//        PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+//
+//        for (int i = 0; i < 10; i++) {
+//            queue1.add(new Random().nextInt(100));
+//        }
+//
+//        System.out.println(queue1);
+//        System.out.println("-------------------------------");
 
         PriorityQueue<Code> codePriorityQueue = new PriorityQueue<>();
 
         for (int i = 0; i < 10; i++) {
-            codePriorityQueue.offer(new Code(new Random().nextDouble(100)));
+            codePriorityQueue.offer(new Code(new Random().nextInt(100)));
         }
 
         for (Code code : codePriorityQueue) {
@@ -31,7 +30,6 @@ public class Exec424 {
 
         for (int i = 0; i < codePriorityQueue.size(); i++) {
             System.out.println(codePriorityQueue.peek());
-
         }
     }
 }
