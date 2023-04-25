a90873dc92a6de2eff61f45f0725575e64867ffd
KirillMar <re4r@bk.ru>
2023-04-25 13:18:56 +0300

    set

--- src/queue/Exec423.java
+++ src/queue/Exec423.java
@@ -1,4 +1,21 @@
 package queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Exec423 {
+    public static void main(String[] args) {
+
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add("AAA");
+        queue.add("CCC");
+        queue.add("EEE");
+        queue.add("DDD");
+        queue.add("BBB");
+        queue.add("FFF");
+
+        System.out.println(queue);
+
+    }
 }
