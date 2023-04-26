733c7ce6f4e8be9692937737fa8e65dc66fe844c
KirillMar <re4r@bk.ru>
2023-04-26 14:57:49 +0300

    nested_classes

--- src/nested_classes/Car.java
+++ src/nested_classes/Car.java
@@ -1,4 +1,37 @@
 package nested_classes;
 
 public class Car {
+    private String color;
+    private int doorCount;
+    private Engine engine;
+
+    public Car(String color, int doorCount, Engine engine) {
+        this.color = color;
+        this.doorCount = doorCount;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Car {" +
+                "color: '" + color + '\'' +
+                ", doorCount: " + doorCount +
+                ", engine: " + engine +
+                '}';
+    }
+
+    private static class Engine {
+        private int horsePower;
+
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "Engine {" +
+                    "horsePower: " + horsePower +
+                    '}';
+        }
+    }
 }
