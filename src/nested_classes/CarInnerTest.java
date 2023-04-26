1778188d2168d0bd07a8d7b946b646c5ab46e4f8
KirillMar <re4r@bk.ru>
2023-04-26 18:36:54 +0300

    nested_classes

--- src/nested_classes/CarInnerTest.java
+++ src/nested_classes/CarInnerTest.java
@@ -1,13 +1,17 @@
 package nested_classes;
 
+import nested_classes.CarInner.Engine;
+
 public class CarInnerTest {
     public static void main(String[] args) {
 
         CarInner car = new CarInner(300, 500);
 
-        CarInner.Engine engine = new CarInner(400, 200).new Engine(700);
+        Engine engine = new CarInner(400, 200).new Engine(700);
+        Engine engine2 = car.new Engine(800);
 
         System.out.println(car);
         System.out.println(engine);
+        System.out.println(engine2);
     }
 }
