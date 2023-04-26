50062f31296cf255932a92b7a55ec759b8f30be6
KirillMar <re4r@bk.ru>
2023-04-26 19:43:04 +0300

    nested_classes

--- src/nested_classes/Exec55.java
+++ src/nested_classes/Exec55.java
@@ -3,12 +3,24 @@ package nested_classes;
 public class Exec55 {
     public static void main(String[] args) {
 
-        MathOperation mathOperation = new MathOperation() {
+        MathOperation mathOperation1 = new MathOperation() {
             @Override
             public int doOperation(int x, int y) {
                 return x * y;
             }
         };
+
+        MathOperation mathOperation2 = new MathOperation() {
+            @Override
+            public int doOperation(int x, int y) {
+                return (int) Math.pow(x, y);
+            }
+        };
+
+        int w = mathOperation1.doOperation(5, 7);
+        System.out.println(w);
+        int t = mathOperation2.doOperation(2, 4);
+        System.out.println(t);
 
     }
 }
