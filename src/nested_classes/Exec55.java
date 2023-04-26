b176a5e7b8f3db080a54e2bc790d9b18a71b4fbc
KirillMar <re4r@bk.ru>
2023-04-26 19:36:29 +0300

    nested_classes

--- src/nested_classes/Exec55.java
+++ src/nested_classes/Exec55.java
@@ -1,4 +1,18 @@
 package nested_classes;
 
 public class Exec55 {
+    public static void main(String[] args) {
+
+        MathOperation mathOperation = new MathOperation() {
+            @Override
+            public int doOperation(int x, int y) {
+                return x * y;
+            }
+        };
+
+    }
+}
+
+interface MathOperation {
+    int doOperation(int x, int y);
 }
