36f873e4272fb98433b82b9aca4967bb7c201784
KirillMar <re4r@bk.ru>
2023-04-26 15:05:32 +0300

    nested_classes

--- src/nested_classes/TestCar.java
+++ src/nested_classes/TestCar.java
@@ -1,4 +1,11 @@
 package nested_classes;
 
 public class TestCar {
+    public static void main(String[] args) {
+
+        Car.Engine engine = new Car.Engine(10000);
+        Car car = new Car("Red", 4, engine);
+
+        System.out.println(car);
+    }
 }
