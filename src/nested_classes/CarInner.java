d2e715564e09761a3fa5de3c4044bdfb87e0a42c
KirillMar <re4r@bk.ru>
2023-04-26 17:51:02 +0300

    nested_classes

--- src/nested_classes/CarInner.java
+++ src/nested_classes/CarInner.java
@@ -12,4 +12,18 @@ public class CarInner {
         return "CarInner: " +
                 "speed: " + speed;
     }
+
+    public class Engine {
+        private int power;
+
+        public Engine(int power) {
+            this.power = power;
+        }
+
+        @Override
+        public String toString() {
+            return "Engine: " +
+                    "power: " + power;
+        }
+    }
 }
