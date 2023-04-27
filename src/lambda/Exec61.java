759561813fff75b6e96660f4a74e41556134bb19
KirillMar <re4r@bk.ru>
2023-04-27 12:45:10 +0300

    lambda

--- src/lambda/Exec61.java
+++ src/lambda/Exec61.java
@@ -1,6 +1,13 @@
 package lambda;
 
 public class Exec61 {
+    public static void main(String[] args) {
+        Student st1 = new Student("AAA", 'f', 27, 1, 5.68);
+        Student st2 = new Student("BBB", 'f', 35, 2, 4.69);
+        Student st3 = new Student("CCC", 'm', 54, 3, 3.54);
+        Student st4 = new Student("DDD", 'f', 25, 4, 5.12);
+        Student st5 = new Student("EEE", 'm', 39, 5, 4.92);
+    }
 
 }
 
