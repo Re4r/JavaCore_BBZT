733e2cb2f095cfea80d7d2e1274f074f5d818c95
KirillMar <re4r@bk.ru>
2023-04-27 15:55:47 +0300

    lambda

--- src/lambda/Exec632.java
+++ src/lambda/Exec632.java
@@ -1,4 +1,20 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.function.Predicate;
+
 public class Exec632 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            integers.add(new Random().nextInt(1000));
+        }
+
+        System.out.println(integers);
+        System.out.println("----------------------------------------------------------------------------------------");
+
+    }
 }
