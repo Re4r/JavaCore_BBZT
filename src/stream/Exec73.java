f939f9e6b36ec20308fe445db6883f8952ab9f45
KirillMar <re4r@bk.ru>
2023-04-28 16:49:36 +0300

    stream

--- src/stream/Exec73.java
+++ src/stream/Exec73.java
@@ -2,6 +2,7 @@ package stream;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Stream;
 
 public class Exec73 {
     public static void main(String[] args) {
@@ -16,8 +17,12 @@ public class Exec73 {
         System.out.println(Arrays.toString(ints));
         System.out.println("----------------------------------------------------------------------------------------");
 
-        Arrays.stream(ints).forEachOrdered(Exec73::accept);
+//        Arrays.stream(ints).forEachOrdered(Tool::multiply);
 //        System.out.println(Arrays.toString(ints));
+
+        Stream<Integer> integerStream = Stream.of(1, 56, 78, 90, 43, 56, 67, 32);
+        Stream<Integer> integerStream1 = integerStream.filter(i -> i > 50);
+        integerStream1.forEach(System.out::println);
 
     }
 
@@ -25,5 +30,12 @@ public class Exec73 {
         value *= 10;
         value /= 3;
         System.out.println(value);
+    }
+}
+
+class Tool {
+    static void multiply(int x) {
+        x *= 100;
+        System.out.println(x);
     }
 }
