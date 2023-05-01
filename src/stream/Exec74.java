aa28004693709ffa347ce2b6157b974c99624c73
KirillMar <re4r@bk.ru>
2023-05-01 19:34:53 +0300

    stream

--- src/stream/Exec74.java
+++ src/stream/Exec74.java
@@ -1,7 +1,16 @@
 package stream;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class Exec74 {
     public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        Supplier<Integer> supplier = () -> new Random().nextInt(100);
 
     }
 }
