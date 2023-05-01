2cc1804b103273236fb0693ebbf8fbfff98860b4
KirillMar <re4r@bk.ru>
2023-05-01 20:22:07 +0300

    stream

--- src/stream/Exec75.java
+++ src/stream/Exec75.java
@@ -1,4 +1,18 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Exec75 {
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(array));
+
+    }
 }
