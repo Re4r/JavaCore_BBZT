244339cc4a95855b73f7f4b8e4a0f8feb63d20ee
KirillMar <re4r@bk.ru>
2023-05-02 20:30:03 +0300

    stream

--- src/stream/Exec718.java
+++ src/stream/Exec718.java
@@ -1,7 +1,18 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Exec718 {
     public static void main(String[] args) {
+
+        double[] array = new double[100];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextDouble(1000);
+        }
+
+//        Arrays.stream(array).forEach(System.out::println);
 
     }
 }
