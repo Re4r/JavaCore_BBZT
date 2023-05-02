7db771e00b65ba5cf4cda7bd282126b9340a8699
KirillMar <re4r@bk.ru>
2023-05-02 12:13:16 +0300

    stream

--- src/stream/Exec76.java
+++ src/stream/Exec76.java
@@ -1,4 +1,25 @@
 package stream;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class Exec76 {
+    public static void main(String[] args) {
+
+        int[] ints = new int[10];
+
+        Supplier<Integer> supplier = new Supplier<Integer>() {
+            @Override
+            public Integer get() {
+                return new Random().nextInt(100);
+            }
+        };
+
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = supplier.get();
+        }
+
+        System.out.println(Arrays.toString(ints));
+    }
 }
