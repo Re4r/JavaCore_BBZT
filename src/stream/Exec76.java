85a140c5af180ed14b14790acdd1a93d652a34a1
KirillMar <re4r@bk.ru>
2023-05-02 12:48:04 +0300

    stream

--- src/stream/Exec76.java
+++ src/stream/Exec76.java
@@ -1,7 +1,6 @@
 package stream;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 import java.util.function.Supplier;
 
 public class Exec76 {
@@ -21,6 +20,7 @@ public class Exec76 {
         }
 
         System.out.println(Arrays.toString(ints));
+        System.out.println("---------------------------------------------------------------------------------------");
 
         int x = Arrays.stream(ints)
                 .filter(v -> v % 10 == 0)
@@ -30,5 +30,76 @@ public class Exec76 {
                 .getAsInt();
 
         System.out.println(x);
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        Set<Mod> modSet = new HashSet<>();
+
+        Supplier<Mod> modSupplier = new Supplier<Mod>() {
+            @Override
+            public Mod get() {
+                return new Mod("AQ".concat(String.valueOf(new Random().nextInt(1000))),
+                        new Random().nextInt(10000));
+            }
+        };
+
+
+        for (int i = 0; i < 10; i++) {
+            modSet.add(modSupplier.get());
+        }
+
+        modSet.forEach(System.out::println);
+        System.out.println("---------------------------------------------------------------------------------------");
+
+
+    }
+}
+
+class Mod {
+    private String name;
+    private int code;
+
+    public Mod(String name, int code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return "Mod{" +
+                "name: '" + name + '\'' +
+                ", code: " + code +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Mod mod)) return false;
+
+        if (getCode() != mod.getCode()) return false;
+        return getName() != null ? getName().equals(mod.getName()) : mod.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + getCode();
+        return result;
     }
 }
