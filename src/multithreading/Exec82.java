e69fe4055c1fe03d230dee56187ca9be359d946a
KirillMar <re4r@bk.ru>
2023-05-03 12:52:01 +0300

    multithreading

--- src/multithreading/Exec82.java
+++ src/multithreading/Exec82.java
@@ -3,15 +3,54 @@ package multithreading;
 public class Exec82 {
     public static void main(String[] args) {
 
-        MyThread1 thread1 = new MyThread1();
-        MyThread2 thread2 = new MyThread2();
-        Thread thread3 = new Thread(new MyThread3());
-        Thread thread4 = new Thread(new MyThread4());
+//        MyThread1 thread1 = new MyThread1();
+//        MyThread2 thread2 = new MyThread2();
+//        Thread thread3 = new Thread(new MyThread3());
+//        Thread thread4 = new Thread(new MyThread4());
+//
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 0; i < 20; i++) {
+//                    System.out.println(i);
+//                }
+//            }
+//        }).start();
+//
+//        new Thread((new Runnable() {
+//            @Override
+//            public void run() {
+//                for (int i = 20; i > 0; i--) {
+//                    System.out.println(i);
+//                }
+//            }
+//        })).start();
+
+        Thread thread5 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 50; i++) {
+                    System.out.println(i);
+                }
+            }
+        });
+
+        Thread thread6 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 50; i > 0; i--) {
+                    System.out.println(i);
+                }
+            }
+        });
+
+        thread5.start();
+        thread6.start();
     }
 }
 
