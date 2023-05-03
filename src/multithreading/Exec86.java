e10c119573d038dc6fe07943be156a13ad02665e
KirillMar <re4r@bk.ru>
2023-05-03 15:37:14 +0300

    multithreading

--- src/multithreading/Exec86.java
+++ src/multithreading/Exec86.java
@@ -1,4 +1,29 @@
 package multithreading;
 
 public class Exec86 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Counter extends Thread {
+    private boolean b = true;
+
+    public boolean isB() {
+        return b;
+    }
+
+    public Counter setB(boolean b) {
+        this.b = b;
+        return this;
+    }
+
+    @Override
+    public void run() {
+        long counter = 0;
+        while (b) {
+            counter++;
+        }
+        System.out.println("Loop finished: " + counter);
+    }
 }
