831aa42b8607e83373507d20fad0ca770205d366
KirillMar <re4r@bk.ru>
2023-05-03 12:13:53 +0300

    multithreading

--- src/multithreading/Exec82.java
+++ src/multithreading/Exec82.java
@@ -3,13 +3,19 @@ package multithreading;
 public class Exec82 {
     public static void main(String[] args) {
 
+        MyThread1 thread1 = new MyThread1();
+        MyThread2 thread2 = new MyThread2();
+
+        thread1.start();
+        thread2.start();
+
     }
 }
 
 class MyThread1 extends Thread {
     @Override
     public void run() {
-        for (int i = 1; i < 1000; i++) {
+        for (int i = 1; i <= 20; i++) {
             System.out.println(i);
         }
     }
@@ -18,7 +24,7 @@ class MyThread1 extends Thread {
 class MyThread2 extends Thread {
     @Override
     public void run() {
-        for (int i = 1000; i < 0; i--) {
+        for (int i = 20; i > 0; i--) {
             System.out.println(i);
         }
     }
