19e264a3237dc78838d82bd60000a6e59b837668
KirillMar <re4r@bk.ru>
2023-05-03 14:53:07 +0300

    multithreading

--- src/multithreading/Exec842.java
+++ src/multithreading/Exec842.java
@@ -3,6 +3,39 @@ package multithreading;
 public class Exec842 {
     public static void main(String[] args) {
 
+        System.out.println(Thread.currentThread().getName() + " - START");
+
+        Thread thread1 = new Thread(new Worker());
+        Thread thread2 = new Thread(new Worker());
+        Thread thread3 = new Thread(new Worker());
+
+        System.out.println(thread1.getState() + " : " + thread1.getName());
+        System.out.println(thread2.getState() + " : " + thread2.getName());
+        System.out.println(thread3.getState() + " : " + thread3.getName());
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        System.out.println(thread1.getState() + " : " + thread1.getName());
+        System.out.println(thread2.getState() + " : " + thread2.getName());
+        System.out.println(thread3.getState() + " : " + thread3.getName());
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+
+            System.out.println(thread1.getState() + " : " + thread1.getName());
+            System.out.println(thread2.getState() + " : " + thread2.getName());
+            System.out.println(thread3.getState() + " : " + thread3.getName());
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(Thread.currentThread().getName() + " - END");
+
     }
 }
 
@@ -11,7 +44,7 @@ class Worker implements Runnable {
     public void run() {
         System.out.println("Work Begins: " + Thread.currentThread().getName());
         try {
-            Thread.sleep(1000);
+            Thread.sleep(4000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
