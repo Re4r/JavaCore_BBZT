b786fe09f553d56ccb9ed595314c6f9f68d85b2c
KirillMar <re4r@bk.ru>
2023-05-03 12:05:05 +0300

    multithreading

--- src/multithreading/Exec82.java
+++ src/multithreading/Exec82.java
@@ -5,3 +5,21 @@ public class Exec82 {
 
     }
 }
+
+class MyThread1 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1000; i < 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
