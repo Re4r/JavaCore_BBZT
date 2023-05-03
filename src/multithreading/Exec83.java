432f13f26239a31ad8d81e651f2ffdc9ad1c09b1
KirillMar <re4r@bk.ru>
2023-05-03 13:11:36 +0300

    multithreading

--- src/multithreading/Exec83.java
+++ src/multithreading/Exec83.java
@@ -3,5 +3,35 @@ package multithreading;
 public class Exec83 {
     public static void main(String[] args) {
 
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread1");
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread2");
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("thread3");
+            }
+        });
+
+        System.out.println(thread1.getName() + " : " + thread1.getPriority());
+        System.out.println(thread2.getName() + " : " + thread2.getPriority());
+        System.out.println(thread3.getName() + " : " + thread3.getPriority());
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+
     }
 }
