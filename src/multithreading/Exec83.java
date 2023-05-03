4373804520e4c5a10252ca176dffacd31b7ee7b5
KirillMar <re4r@bk.ru>
2023-05-03 13:17:48 +0300

    multithreading

--- src/multithreading/Exec83.java
+++ src/multithreading/Exec83.java
@@ -23,14 +23,28 @@ public class Exec83 {
                 System.out.println("thread3");
             }
         });
+        System.out.println("-----------------------------------------------");
+        System.out.println(thread1.getName() + " : " + thread1.getPriority());
+        System.out.println(thread2.getName() + " : " + thread2.getPriority());
+        System.out.println(thread3.getName() + " : " + thread3.getPriority());
+        System.out.println("-----------------------------------------------");
+
+        thread1.setName("threadOne");
+        thread1.setPriority(3);
+        thread2.setName("threadTwo");
+        thread2.setPriority(7);
+        thread3.setName("threadThree");
+        thread3.setPriority(10);
 
         System.out.println(thread1.getName() + " : " + thread1.getPriority());
         System.out.println(thread2.getName() + " : " + thread2.getPriority());
         System.out.println(thread3.getName() + " : " + thread3.getPriority());
+        System.out.println("-----------------------------------------------");
 
         thread1.start();
         thread2.start();
         thread3.start();
+
 
 
     }
