b13041b2b01bea094b4ece912b4b8f2017ab93a4
KirillMar <re4r@bk.ru>
2023-05-03 19:19:02 +0300

    multithreading

--- src/multithreading/Exec872.java
+++ src/multithreading/Exec872.java
@@ -4,18 +4,22 @@ public class Exec872 {
     static int counter;
 
     public static void main(String[] args) {
+
         Repeater repeaterThread1 = new Repeater();
         Repeater repeaterThread2 = new Repeater();
         Repeater repeaterThread3 = new Repeater();
+        Repeater repeaterThread4 = new Repeater();
 
         repeaterThread1.start();
         repeaterThread2.start();
         repeaterThread3.start();
+        repeaterThread4.start();
 
         try {
             repeaterThread1.join();
             repeaterThread2.join();
             repeaterThread3.join();
+            repeaterThread4.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
@@ -29,7 +33,7 @@ public class Exec872 {
 class Repeater extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100000; i++) {
             Exec872.counter++;
             System.out.println(Thread.currentThread().getName() + " > " + Exec872.counter);
         }
