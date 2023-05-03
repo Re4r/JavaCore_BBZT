a8b90eb5fc8868f8d5f464a03703ed661f0bfe01
KirillMar <re4r@bk.ru>
2023-05-03 20:22:21 +0300

    multithreading

--- src/multithreading/Exec88.java
+++ src/multithreading/Exec88.java
@@ -3,5 +3,36 @@ package multithreading;
 public class Exec88 {
     public static void main(String[] args) {
 
+        Sync sync = new Sync();
+
+        Thread thread1 = new Thread(sync);
+        Thread thread2 = new Thread(sync);
+        Thread thread3 = new Thread(sync);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+}
+
+class Counter2 {
+    static int counter;
+
+}
+
+class Sync implements Runnable {
+
+    public void doWork() {
+        synchronized (this) {
+            Counter2.counter++;
+            System.out.println(Counter2.counter);
+        }
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            doWork();
+        }
     }
 }
