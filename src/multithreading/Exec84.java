16cceaa845e299455e56cb8beaf5d65b89e90ec6
KirillMar <re4r@bk.ru>
2023-05-03 13:52:03 +0300

    multithreading

--- src/multithreading/Exec84.java
+++ src/multithreading/Exec84.java
@@ -3,5 +3,27 @@ package multithreading;
 public class Exec84 {
     public static void main(String[] args) {
 
+        Thread thread1 = new Thread(new Robo());
+        Thread thread2 = new Thread(new Robo());
+        Thread thread3 = new Thread(new Robo());
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+    }
+}
+
+class Robo implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            try {
+                Thread.sleep(1_000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
     }
 }
