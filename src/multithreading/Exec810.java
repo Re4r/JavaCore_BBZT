8f6eb81998e717c1920591b5bfc376edb813962f
KirillMar <re4r@bk.ru>
2023-05-04 18:35:59 +0300

    multithreading

--- src/multithreading/Exec810.java
+++ src/multithreading/Exec810.java
@@ -3,7 +3,14 @@ package multithreading;
 public class Exec810 {
     public static final Object LOCK1 = new Object();
     public static final Object LOCK2 = new Object();
+
     public static void main(String[] args) {
+
+        Thex thexThread = new Thex();
+        ThexOne thexOneThread = new ThexOne();
+
+        thexThread.start();
+        thexOneThread.start();
 
 
     }
@@ -27,13 +34,13 @@ class Thex extends Thread {
 class ThexOne extends Thread {
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " Trying to cath monitor of object LOCK2...");
-        synchronized (Exec810.LOCK2) {
-            System.out.println(Thread.currentThread().getName() + " Monitor of object LOCK2 - Catched!");
-            System.out.println(Thread.currentThread().getName() + " Trying to cath monitor of object LOCK1...");
-            synchronized (Exec810.LOCK1) {
-                System.out.println(Thread.currentThread().getName() + " Monitor of object LOCK2 - Catched! "
-                        + "Monitor of object LOCK1 - Catched!");
+        System.out.println(Thread.currentThread().getName() + " Trying to cath monitor of object LOCK1...");
+        synchronized (Exec810.LOCK1) {
+            System.out.println(Thread.currentThread().getName() + " Monitor of object LOCK1 - Catched!");
+            System.out.println(Thread.currentThread().getName() + " Trying to cath monitor of object LOCK2...");
+            synchronized (Exec810.LOCK2) {
+                System.out.println(Thread.currentThread().getName() + " Monitor of object LOCK1 - Catched! "
+                        + "Monitor of object LOCK2 - Catched!");
             }
         }
     }
