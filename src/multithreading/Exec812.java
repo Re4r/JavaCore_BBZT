2e2ccad3317549e239d0c1b9b3b17df1d890bc60
KirillMar <re4r@bk.ru>
2023-05-04 20:19:57 +0300

    multithreading

--- src/multithreading/Exec812.java
+++ src/multithreading/Exec812.java
@@ -3,5 +3,37 @@ package multithreading;
 public class Exec812 {
     public static void main(String[] args) {
 
+
+
+    }
+}
+
+class UserThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is daemon: " + isDaemon());
+        for (char i = 'A'; i <= 'J'; i++) {
+            try {
+                sleep(1000);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class DaemonThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " is daemon: " + isDaemon());
+        for (char i = 1; i <= 1000; i++) {
+            try {
+                sleep(500);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
