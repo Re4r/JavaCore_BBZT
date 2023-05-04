6ae27495e6306416dc39ae894f8ea5788a0dee05
KirillMar <re4r@bk.ru>
2023-05-04 14:49:01 +0300

    multithreading

--- src/multithreading/Exec89.java
+++ src/multithreading/Exec89.java
@@ -22,4 +22,18 @@ class Market {
             notify();
         }
     }
+
+    synchronized void putBread() {
+        while (breadCount != 0 && breadCount <= 5) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            breadCount++;
+            System.out.println("Producer added 1 bread to the market");
+            System.out.println("Qantity of bread in the market: " + breadCount);
+            notify();
+        }
+    }
 }
