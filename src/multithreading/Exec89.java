b9bd0974617c70b39d72e8d96dd7b0ce4684b995
KirillMar <re4r@bk.ru>
2023-05-04 14:54:35 +0300

    multithreading

--- src/multithreading/Exec89.java
+++ src/multithreading/Exec89.java
@@ -24,7 +24,7 @@ class Market {
     }
 
     synchronized void putBread() {
-        while (breadCount != 0 && breadCount <= 5) {
+        while (breadCount >= 5) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -34,6 +34,37 @@ class Market {
             System.out.println("Producer added 1 bread to the market");
             System.out.println("Qantity of bread in the market: " + breadCount);
             notify();
+        }
+    }
+}
+
+class Producer implements Runnable {
+
+    private Market market;
+
+    public Producer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+
+class Customer implements Runnable {
+    private Market market;
+
+    public Customer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
         }
     }
 }
