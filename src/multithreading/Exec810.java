33dfb8bc98e0470b74e98a4d42dc36023fe22f5e
KirillMar <re4r@bk.ru>
2023-05-04 18:15:47 +0300

    multithreading

---
+++ src/multithreading/Exec810.java
@@ -0,0 +1,25 @@
+package multithreading;
+
+public class Exec810 {
+    public static final Object LOCK1 = new Object();
+    public static final Object LOCK2 = new Object();
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class Thex extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Trying to cath monitor of object LOCK1...");
+        synchronized (Exec810.LOCK1) {
+            System.out.println(Thread.currentThread().getName() + " Monitor of object LOCK1 - Catched!");
+            System.out.println(Thread.currentThread().getName() + " Trying to cath monitor of object LOCK2...");
+            synchronized (Exec810.LOCK2) {
+                System.out.println(Thread.currentThread().getName() + " Monitor of object LOCK1 - Catched! "
+                        + "Monitor of object LOCK2 - Catched!");
+            }
+        }
+    }
+}
