88c706e1104fc70aa7955ff1646ba4761309a381
KirillMar <re4r@bk.ru>
2023-05-04 20:36:08 +0300

    multithreading

--- src/multithreading/Exec813.java
+++ src/multithreading/Exec813.java
@@ -2,3 +2,15 @@ package multithreading;
 
 public class Exec813 {
 }
+
+class InterruptedThread extends Thread {
+    private static double sqrtSum;
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            sqrtSum += Math.sqrt(i);
+        }
+        System.out.println(sqrtSum);
+    }
+}
