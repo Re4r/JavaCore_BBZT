004ba2da8c7ec2ef11807f6bebcf2163c6965557
KirillMar <re4r@bk.ru>
2023-05-04 14:43:15 +0300

    multithreading

--- src/multithreading/Exec89.java
+++ src/multithreading/Exec89.java
@@ -5,3 +5,21 @@ public class Exec89 {
 
     }
 }
+
+class Market {
+    private int breadCount;
+
+    synchronized void getBread() {
+        while (breadCount == 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            breadCount--;
+            System.out.println("Customer buy 1 bread");
+            System.out.println("Qantity of bread in the market: " + breadCount);
+            notify();
+        }
+    }
+}
