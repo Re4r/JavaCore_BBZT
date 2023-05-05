07edd69199c2b1938ee22d2ae8fcb4aef6b63270
KirillMar <re4r@bk.ru>
2023-05-05 14:53:07 +0300

    multithreading

--- src/multithreading/Exec815.java
+++ src/multithreading/Exec815.java
@@ -2,7 +2,6 @@ package multithreading;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Exec815 {
     public static void main(String[] args) {
