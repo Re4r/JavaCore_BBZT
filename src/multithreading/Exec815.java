c323ce2baf4c8c444219bd7109ece9aba080e609
KirillMar <re4r@bk.ru>
2023-05-05 14:21:50 +0300

    multithreading

--- src/multithreading/Exec815.java
+++ src/multithreading/Exec815.java
@@ -2,3 +2,25 @@ package multithreading;
 
 public class Exec815 {
 }
+
+class BlackFox implements Runnable {
+
+    private static int counter;
+
+    @Override
+    public void run() {
+
+        System.out.println(Thread.currentThread().getName() + " > starts work...");
+
+        for (int i = 0; i < 3; i++) {
+            counter++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName() + " > ends work..." + " counter = " + counter);
+    }
+}
