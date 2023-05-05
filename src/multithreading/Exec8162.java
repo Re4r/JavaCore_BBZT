45ec9957dea1fc596f0fb24d6f11acbbc1a081e7
KirillMar <re4r@bk.ru>
2023-05-05 17:07:41 +0300

    multithreading

--- src/multithreading/Exec8162.java
+++ src/multithreading/Exec8162.java
@@ -1,9 +1,16 @@
 package multithreading;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Exec8162 {
     public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Factorial2 factorial2 = new Factorial2(5);
+        Future<Integer> future = executorService.submit(factorial2);
 
     }
 }
