a4a9ef264c47d63625e8b6f020fa2e3820f1fd36
KirillMar <re4r@bk.ru>
2023-05-05 13:24:39 +0300

    multithreading

--- src/multithreading/Exec814.java
+++ src/multithreading/Exec814.java
@@ -1,4 +1,22 @@
 package multithreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Exec814 {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+    }
+}
+
+class Fox implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " > active...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
