7bb94e89ec091b000c52489da11989ad81554895
KirillMar <re4r@bk.ru>
2023-05-05 17:00:05 +0300

    multithreading

--- src/multithreading/Exec816.java
+++ src/multithreading/Exec816.java
@@ -1,11 +1,30 @@
 package multithreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Exec816 {
 
     static int factorialResult;
 
     public static void main(String[] args) {
 
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Factorial factorial = new Factorial(5);
+
+        executorService.execute(factorial);
+
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(factorialResult);
     }
 }
 
@@ -23,7 +42,7 @@ final class Factorial implements Runnable {
             return;
         } else {
             int result = 1;
-            for (int i = 1; i < f; i++) {
+            for (int i = 1; i <= f; i++) {
                 result *= i;
             }
             Exec816.factorialResult = result;
