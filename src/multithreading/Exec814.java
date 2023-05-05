07c5257e16a36f07a89308535927f4b3bfc31fbb
KirillMar <re4r@bk.ru>
2023-05-05 13:42:06 +0300

    multithreading

--- src/multithreading/Exec814.java
+++ src/multithreading/Exec814.java
@@ -2,14 +2,25 @@ package multithreading;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Exec814 {
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread().getName() + " Starts");
+
+        System.out.println(Thread.currentThread().getName() + " Starts...");
+
         ExecutorService executorService = Executors.newFixedThreadPool(5);
 
         for (int i = 0; i < 10; i++) {
             executorService.execute(new Fox());
+        }
+
+        executorService.shutdown();
+
+        try {
+            executorService.awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
         System.out.println(Thread.currentThread().getName() + " Ends");
@@ -19,11 +30,12 @@ public class Exec814 {
 class Fox implements Runnable {
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " > in progress...");
+        System.out.println(Thread.currentThread().getName() + " > starts work...");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        System.out.println(Thread.currentThread().getName() + " > ends work...");
     }
 }
