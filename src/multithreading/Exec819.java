78ad9bef9c5cd7e22c5d9c67a0b186d89daea431
KirillMar <re4r@bk.ru>
2023-05-08 19:19:13 +0300

    multithreading

--- src/multithreading/Exec819.java
+++ src/multithreading/Exec819.java
@@ -1,7 +1,26 @@
 package multithreading;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Exec819 {
+    static CountDownLatch countDownLatch;
+
+    static {
+        countDownLatch = new CountDownLatch(3);
+    }
+
     public static void main(String[] args) {
 
+    }
+
+    private static void marketStaffOnPlace() {
+        try {
+            Thread.sleep(2000);
+            System.out.println("Market Staff came to work...");
+            countDownLatch.countDown();
+            System.out.println("cdl: " + countDownLatch);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
