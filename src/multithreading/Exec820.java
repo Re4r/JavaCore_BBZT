7a0e4fad8f5024c7efe7343cdccf5655face9210
KirillMar <re4r@bk.ru>
2023-05-08 19:52:27 +0300

    multithreading

--- src/multithreading/Exec820.java
+++ src/multithreading/Exec820.java
@@ -1,4 +1,26 @@
 package multithreading;
 
+import java.util.List;
+import java.util.concurrent.Exchanger;
+
 public class Exec820 {
+    public static void main(String[] args) {
+
+    }
+}
+
+enum Action {
+    STONE,
+    SCISSORS,
+    PAPPER
+}
+
+class Playser {
+    private Exchanger<Action> actionExchanger;
+    private List<Action> actionList;
+
+    public Playser(Exchanger<Action> actionExchanger, List<Action> actionList) {
+        this.actionExchanger = actionExchanger;
+        this.actionList = actionList;
+    }
 }
