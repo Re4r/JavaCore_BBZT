c7a5165a24c53a55bf27c64bd0d04628c9980b78
KirillMar <re4r@bk.ru>
2023-05-08 17:54:59 +0300

    multithreading

--- src/multithreading/Exec817.java
+++ src/multithreading/Exec817.java
@@ -1,7 +1,12 @@
 package multithreading;
 
 public class Exec817 {
+
+    private static long value = 1_000_000_000L;
+    private static long sum;
+
     public static void main(String[] args) {
+
 
     }
 }
