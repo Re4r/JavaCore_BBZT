fe119b90d85d2a6fdd680036cbf96382dd1ce47f
KirillMar <re4r@bk.ru>
2023-05-08 17:49:08 +0300

    multithreading

--- src/multithreading/Exec816.java
+++ src/multithreading/Exec816.java
@@ -2,6 +2,7 @@ package multithreading;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class Exec816 {
@@ -16,6 +17,10 @@ public class Exec816 {
 
         executorService.execute(factorial);
 
+        Future future = executorService.submit(factorial);
+
+        System.out.println(future.isDone());
+
         executorService.shutdown();
 
         try {
@@ -23,6 +28,8 @@ public class Exec816 {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+
+        System.out.println(future.isDone());
 
         System.out.println(factorialResult);
     }
--- src/multithreading/Exec8162.java
+++ src/multithreading/Exec8162.java
@@ -9,12 +9,16 @@ public class Exec8162 {
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        Factorial2 factorial2 = new Factorial2(5);
+        Factorial2 factorial2 = new Factorial2(6);
 
         Future<Integer> future = executorService.submit(factorial2);
 
         try {
+            System.out.println(future.isDone());
+            System.out.println("Wants to get result...");
             factorialResult = future.get();
+            System.out.println("Got result...");
+            System.out.println(future.isDone());
         } catch (InterruptedException e) {
             e.printStackTrace();
             System.out.println(e.getCause());
@@ -45,6 +49,7 @@ class Factorial2 implements Callable<Integer> {
         int result = 1;
         for (int i = 1; i <= f; i++) {
             result *= i;
+            Thread.sleep(1000);
         }
         return result;
     }
