3ef5a3c2b58a4472f12c82bdbd4195ffbbfade07
KirillMar <re4r@bk.ru>
2023-05-08 18:05:59 +0300

    multithreading

--- src/multithreading/Exec817.java
+++ src/multithreading/Exec817.java
@@ -1,6 +1,11 @@
 package multithreading;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Exec817 {
 
@@ -8,6 +13,17 @@ public class Exec817 {
     private static long sum;
 
     public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        List<Future<Long>> futureList = new ArrayList<>();
+
+        long valueDevidedBy10 = value / 10;
+
+        for (int i = 0; i < 10; i++) {
+            long from = valueDevidedBy10 * i + i;
+            long to = valueDevidedBy10  * (i + 1);
+        }
 
 
     }
@@ -23,4 +39,12 @@ class PartialSum implements Callable<Long> {
         this.to = to;
     }
 
+    @Override
+    public Long call() {
+        for (long i = from; i <= to; i++) {
+            localSum += i;
+        }
+        System.out.println("Sum from: " + from + " to " + to + " > " + localSum);
+        return localSum;
+    }
 }
