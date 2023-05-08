e098603852a5ed04f6ca1638ec0ced6f221f05ee
KirillMar <re4r@bk.ru>
2023-05-08 20:19:50 +0300

    multithreading

--- src/multithreading/Exec820.java
+++ src/multithreading/Exec820.java
@@ -1,10 +1,26 @@
 package multithreading;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Exchanger;
 
 public class Exec820 {
     public static void main(String[] args) {
+
+        Exchanger<Action> actionExchanger = new Exchanger<>();
+
+        List<Action> actionList1 = new ArrayList<>();
+
+        actionList1.add(Action.STONE);
+        actionList1.add(Action.PAPPER);
+        actionList1.add(Action.SCISSORS);
+
+        List<Action> actionList2 = new ArrayList<>();
+
+        actionList2.add(Action.PAPPER);
+        actionList2.add(Action.SCISSORS);
+        actionList2.add(Action.STONE);
+
 
     }
 }
@@ -28,7 +44,10 @@ class Playser extends Thread {
         if ((action == Action.STONE && reply == Action.SCISSORS)
                 || (action == Action.SCISSORS && reply == Action.PAPPER)
                 || (action == Action.PAPPER && reply == Action.STONE)) {
-            System.out.println(Thread.currentThread().getName() + " > WINS");
+
+            System.out.println(Thread.currentThread().getName() + " > WINS!!!");
+        } else {
+            System.out.println("NOBODY WINS...");
         }
     }
 
@@ -39,6 +58,7 @@ class Playser extends Thread {
             try {
                 reply = actionExchanger.exchange(action);
                 whoWins(action, reply);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
