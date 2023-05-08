a3bb32e40827deb9dd93e089e617add9d23fbbbd
KirillMar <re4r@bk.ru>
2023-05-08 18:51:46 +0300

    multithreading

--- src/multithreading/Exec818.java
+++ src/multithreading/Exec818.java
@@ -1,4 +1,34 @@
 package multithreading;
 
+import java.util.concurrent.Semaphore;
+
 public class Exec818 {
+    public static void main(String[] args) {
+
+        Semaphore phoneBox = new Semaphore(2);
+
+    }
+}
+
+class Person extends Thread {
+    private Semaphore phoneBox;
+
+    public Person(Semaphore phoneBox) {
+        this.phoneBox = phoneBox;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " Waiting...");
+            phoneBox.acquire();
+            System.out.println(Thread.currentThread().getName() + " Using device...");
+            Thread.sleep(2000);
+            System.out.println(Thread.currentThread().getName() + " Finished...");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            phoneBox.release();
+        }
+    }
 }
