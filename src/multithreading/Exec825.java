b3b69c051a01b373be3f0cb031830e04cd01ff61
KirillMar <re4r@bk.ru>
2023-05-09 18:31:50 +0300

    multithreading

--- src/multithreading/Exec825.java
+++ src/multithreading/Exec825.java
@@ -1,4 +1,27 @@
 package multithreading;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Exec825 {
+    public static void main(String[] args) {
+
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(10);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(new Random().nextInt(100));
+                    System.out.println(Thread.currentThread().getName() + " > put number");
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+
+    }
 }
