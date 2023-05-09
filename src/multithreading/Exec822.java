60836845687540a92966977dbed5c941e406725b
KirillMar <re4r@bk.ru>
2023-05-09 15:13:07 +0300

    multithreading

--- src/multithreading/Exec822.java
+++ src/multithreading/Exec822.java
@@ -1,4 +1,17 @@
 package multithreading;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Exec822 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> source = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            source.add(new Random().nextInt(100));
+        }
+
+        System.out.println(source);
+    }
 }
