e3c80971ec46e9bc5e1660bbae8299a87ae349ac
KirillMar <re4r@bk.ru>
2023-05-09 17:35:11 +0300

    multithreading

--- src/multithreading/Exec824.java
+++ src/multithreading/Exec824.java
@@ -1,4 +1,17 @@
 package multithreading;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Exec824 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> integerArrayList = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            integerArrayList.add(new Random().nextInt(100));
+        }
+
+        System.out.println(integerArrayList);
+    }
 }
