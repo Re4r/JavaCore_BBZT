8fa1ea383abb18498f27330cac31fbb72b6c10e1
KirillMar <re4r@bk.ru>
2023-05-09 15:29:56 +0300

    multithreading

---
+++ src/multithreading/Exec8222.java
@@ -0,0 +1,16 @@
+package multithreading;
+
+import java.util.ArrayList;
+
+public class Exec8222 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            arrayList.add(i);
+        }
+
+
+    }
+}
