bb4179b812dd50cbd5ec9f4052a966b692dabf58
KirillMar <re4r@bk.ru>
2023-05-09 20:16:59 +0300

    multithreading

--- src/multithreading/Exec825.java
+++ src/multithreading/Exec825.java
@@ -14,9 +14,9 @@ public class Exec825 {
                 try {
                     Integer x = new Random().nextInt(100);
                     arrayBlockingQueue.put(x);
-                    System.out.println(Thread.currentThread().getName() + " > put number: " + x);
+                    System.out.println(Thread.currentThread().getName() + " > put number: " + x + " : " + arrayBlockingQueue);
                     System.out.println("------------------------------------------------");
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -28,9 +28,9 @@ public class Exec825 {
             while (true) {
                 try {
                     Integer x = arrayBlockingQueue.take();
-                    System.out.println(Thread.currentThread().getName() + " > took number: " + x);
+                    System.out.println(Thread.currentThread().getName() + " > took number: " + x + " : " + arrayBlockingQueue);
                     System.out.println("------------------------------------------------");
-                    Thread.sleep(1000);
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
