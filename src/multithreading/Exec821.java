32f4e4eb715bcfd61bf8f93055cb1f10c40c289f
KirillMar <re4r@bk.ru>
2023-05-09 14:49:46 +0300

    multithreading

--- src/multithreading/Exec821.java
+++ src/multithreading/Exec821.java
@@ -3,7 +3,11 @@ package multithreading;
 public class Exec821 {
     private static int counter;
 
-    static void increment() {
+    public static int getCounter() {
+        return counter;
+    }
+
+    static synchronized void increment() {
         counter++;
     }
 
@@ -11,18 +15,21 @@ public class Exec821 {
 
         Thread thread1 = new Thread(new Run2());
         Thread thread2 = new Thread(new Run2());
+        Thread thread3 = new Thread(new Run2());
 
         thread1.start();
         thread2.start();
+        thread3.start();
 
         try {
             thread1.join();
             thread2.join();
+            thread3.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        System.out.println(counter);
+        System.out.println("Counter = " + counter);
 
     }
 }
@@ -30,6 +37,14 @@ public class Exec821 {
 class Run2 implements Runnable {
     @Override
     public void run() {
-        Exec821.increment();
+        System.out.println(Thread.currentThread().getName() + " > starts work...");
+        for (int i = 0; i < 1_000_000; i++) {
+            Exec821.increment();
+        }
+        System.out.println(new StringBuilder()
+                .append(Thread.currentThread().getName())
+                .append(" : ")
+                .append(Exec821.getCounter())
+                .append(" > ends work"));
     }
 }
