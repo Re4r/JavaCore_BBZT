6ca553f76ac80b8e229111f70ebfaa56d51d61db
KirillMar <re4r@bk.ru>
2023-05-09 15:28:37 +0300

    multithreading

--- src/multithreading/Exec822.java
+++ src/multithreading/Exec822.java
@@ -1,6 +1,8 @@
 package multithreading;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Exec822 {
@@ -10,18 +12,30 @@ public class Exec822 {
 
         ArrayList<Integer> target = new ArrayList<>();
 
+        List<Integer> syncList = Collections.synchronizedList(target);
+
         for (int i = 0; i < 10; i++) {
             source.add(new Random().nextInt(100));
         }
 
         System.out.println(source);
 
-        Runnable runnable = () -> target.addAll(source);
+        Runnable runnable = () -> syncList.addAll(source);
 
         Thread thread1 = new Thread(runnable);
         Thread thread2 = new Thread(runnable);
 
+        thread1.start();
+        thread2.start();
 
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(syncList);
 
 
     }
