e83820ba515e18205fddb8c80c8d40d834d8c803
KirillMar <re4r@bk.ru>
2023-05-09 17:44:36 +0300

    multithreading

--- src/multithreading/Exec824.java
+++ src/multithreading/Exec824.java
@@ -1,6 +1,7 @@
 package multithreading;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class Exec824 {
@@ -13,5 +14,44 @@ public class Exec824 {
         }
 
         System.out.println(integerArrayList);
+
+        System.out.println("--------------------------------------------------------------------------------------");
+
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                Iterator<Integer> iterator = integerArrayList.iterator();
+                while (iterator.hasNext()) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(iterator.next());
+                }
+            }
+        };
+
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                Iterator<Integer> iterator = integerArrayList.iterator();
+                while (iterator.hasNext()) {
+                    try {
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Integer x = iterator.next();
+                    if (x > 50) {
+                        iterator.remove();
+                    } else {
+                        integerArrayList.set(integerArrayList.indexOf(x), new Random().nextInt(1000));
+                    }
+                }
+            }
+        };
+
+
     }
 }
