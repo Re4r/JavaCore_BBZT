e81aae0d4c04d02ecfb3a3c8f6c2093bd7616dd0
KirillMar <re4r@bk.ru>
2023-05-09 16:36:32 +0300

    multithreading

--- src/multithreading/Exec823.java
+++ src/multithreading/Exec823.java
@@ -1,4 +1,23 @@
 package multithreading;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exec823 {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+
+        hashMap.put(1, "AAA");
+        hashMap.put(2, "BBB");
+        hashMap.put(3, "CCC");
+        hashMap.put(4, "DDD");
+        hashMap.put(5, "EEE");
+
+        for (Map.Entry entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+
+    }
 }
