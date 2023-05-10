6dfeb0b74123090ebcf9fc2be77f0901336c34ca
KirillMar <re4r@bk.ru>
2023-05-10 19:31:08 +0300

    streams

--- src/streams/Exec92.java
+++ src/streams/Exec92.java
@@ -1,7 +1,18 @@
 package streams;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exec92 {
     public static void main(String[] args) {
+
+        try (FileReader fileReader = new FileReader("dest.txt")) {
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
