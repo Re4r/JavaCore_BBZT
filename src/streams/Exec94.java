742fa619f7dfa209ad74a75b0eb0b5c880d83b4b
KirillMar <re4r@bk.ru>
2023-05-10 20:06:05 +0300

    streams

--- src/streams/Exec94.java
+++ src/streams/Exec94.java
@@ -1,14 +1,14 @@
 package streams;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Exec94 {
     public static void main(String[] args) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("dest.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("dest.txt"));
+             BufferedWriter writer = new BufferedWriter(new FileWriter("dest2.txt"))) {
+
+
 
 
 
