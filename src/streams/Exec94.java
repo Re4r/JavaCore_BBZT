c2f1c0227ce9f79858ee9e30d5b955ca25d9b470
KirillMar <re4r@bk.ru>
2023-05-10 20:03:22 +0300

    streams

--- src/streams/Exec94.java
+++ src/streams/Exec94.java
@@ -1,7 +1,20 @@
 package streams;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exec94 {
     public static void main(String[] args) {
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("dest.txt"))) {
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
