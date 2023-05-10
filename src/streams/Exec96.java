e60fac2647e98cb6edbed816193ffbe60a204f63
KirillMar <re4r@bk.ru>
2023-05-10 21:12:25 +0300

    streams

--- src/streams/Exec96.java
+++ src/streams/Exec96.java
@@ -1,4 +1,20 @@
 package streams;
 
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Exec96 {
+    public static void main(String[] args) {
+
+        try (DataOutputStream dataOutputStream =
+                     new DataOutputStream(new FileOutputStream("my_test.bin"))) {
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
