1b30721fa69fe1ed0d6804acc9e4b203ecd52d19
KirillMar <re4r@bk.ru>
2023-05-10 20:23:06 +0300

    streams

--- src/streams/Exec95.java
+++ src/streams/Exec95.java
@@ -1,4 +1,24 @@
 package streams;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Exec95 {
+    public static void main(String[] args) {
+
+        try (FileInputStream fileInputStream =
+                     new FileInputStream("C:\\Users\\re4r\\OneDrive\\Рабочий стол\\TEMP\\testpict.jpg");
+             FileOutputStream fileOutputStream =
+                     new FileOutputStream("destpict.jpg")) {
+
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
