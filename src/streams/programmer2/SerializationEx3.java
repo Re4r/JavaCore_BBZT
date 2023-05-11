04f6db37c250d71b3ead9755f79f20c010e80bde
KirillMar <re4r@bk.ru>
2023-05-11 17:52:34 +0300

    streams

--- src/streams/programmer2/SerializationEx3.java
+++ src/streams/programmer2/SerializationEx3.java
@@ -1,7 +1,30 @@
 package streams.programmer2;
 
+import streams.programmer1.Employee;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class SerializationEx3 {
     public static void main(String[] args) {
 
+        Employee employee;
+
+        try (ObjectInputStream objectInputStream =
+                     new ObjectInputStream(new FileInputStream("employee2.bin"))) {
+
+            employee = (Employee) objectInputStream.readObject();
+
+            System.out.println(employee);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
