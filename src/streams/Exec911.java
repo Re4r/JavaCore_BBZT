f9ccd27bfedb1385748127b4e8032209af5889a1
KirillMar <re4r@bk.ru>
2023-05-11 20:40:27 +0300

    streams

--- src/streams/Exec911.java
+++ src/streams/Exec911.java
@@ -1,4 +1,26 @@
 package streams;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 public class Exec911 {
+    public static void main(String[] args) {
+
+        try (RandomAccessFile file = new RandomAccessFile("dest.txt", "rw")) {
+            try (FileChannel channel = file.getChannel()) {
+
+                ByteBuffer buffer = ByteBuffer.allocate(25);
+
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
