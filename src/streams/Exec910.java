81e1d422fb2496f0b826ec073eb0b9d4a5378dc3
KirillMar <re4r@bk.ru>
2023-05-11 19:40:04 +0300

    streams

--- src/streams/Exec910.java
+++ src/streams/Exec910.java
@@ -7,6 +7,24 @@ public class Exec910 {
 
         File file = new File("dest.txt");
         File folder = new File("D:\\FileTest");
+        File file2 = new File("D:\\FileTest\\dest3.txt");
+
+        System.out.println("------------------------");
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file2.getAbsolutePath());
+        System.out.println("------------------------");
+        System.out.println(folder.getAbsolutePath());
+        System.out.println("------------------------");
+        System.out.println(file.isAbsolute());
+        System.out.println(folder.isAbsolute());
+        System.out.println("------------------------");
+        System.out.println(file.isDirectory());
+        System.out.println(folder.isDirectory());
+        System.out.println("------------------------");
+        System.out.println(file.exists());
+        System.out.println(file2.exists());
+        System.out.println(folder.exists());
+        System.out.println("------------------------");
 
     }
 }
