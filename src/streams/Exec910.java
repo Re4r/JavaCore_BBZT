e6b9d362dc94be7450ef4ecec57d921c64a6194b
KirillMar <re4r@bk.ru>
2023-05-11 19:31:30 +0300

    streams

--- src/streams/Exec910.java
+++ src/streams/Exec910.java
@@ -1,7 +1,12 @@
 package streams;
 
+import java.io.File;
+
 public class Exec910 {
     public static void main(String[] args) {
+
+        File file = new File("dest.txt");
+        File folder = new File("D:\\FileTest");
 
     }
 }
