e2cb27d671e0a8a6b2cc9abafa5c461a46d591ae
KirillMar <re4r@bk.ru>
2023-05-11 19:13:39 +0300

    streams

--- src/streams/Exec99.java
+++ src/streams/Exec99.java
@@ -1,7 +1,20 @@
 package streams;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 public class Exec99 {
     public static void main(String[] args) {
+
+        try (RandomAccessFile randomAccessFile =
+                     new RandomAccessFile("dest.txt", "rw")) {
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
