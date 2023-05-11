c47516d4402736f2fb1a9708d6d1f2ef113d6a98
KirillMar <re4r@bk.ru>
2023-05-11 19:21:55 +0300

    streams

--- src/streams/Exec99.java
+++ src/streams/Exec99.java
@@ -10,6 +10,16 @@ public class Exec99 {
         try (RandomAccessFile randomAccessFile =
                      new RandomAccessFile("dest.txt", "rw")) {
 
+            String s = randomAccessFile.readLine();
+            System.out.println(s);
+            System.out.println("-----------------------");
+            System.out.println(randomAccessFile.length() + " - byte");
+            randomAccessFile.seek(100);
+            String s2 = randomAccessFile.readLine();
+            System.out.println(s2);
+            System.out.println("-----------------------");
+            System.out.println(randomAccessFile.getFilePointer());
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
