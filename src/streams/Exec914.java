4f9e1c10741b092fa8caad4bef583b113a3a0387
KirillMar <re4r@bk.ru>
2023-05-12 11:45:10 +0300

    io/nio

--- src/streams/Exec914.java
+++ src/streams/Exec914.java
@@ -1,4 +1,14 @@
 package streams;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Exec914 {
+    public static void main(String[] args) {
+
+        Path filePath = Paths.get("test15.txt");
+        Path directoryPath = Paths.get("D:\\FileTest\\M");
+
+
+    }
 }
