5aa7bd84d8112e3a63517c93e9409ba36b565942
KirillMar <re4r@bk.ru>
2023-05-12 11:08:23 +0300

    io/nio

--- src/streams/Exec913.java
+++ src/streams/Exec913.java
@@ -3,6 +3,7 @@ package streams;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class Exec913 {
@@ -10,6 +11,17 @@ public class Exec913 {
 
         try (RandomAccessFile file = new RandomAccessFile("dest.txt", "r");
              FileChannel channel = file.getChannel()) {
+
+            ByteBuffer buffer;
+            buffer = ByteBuffer.allocate(Math.toIntExact(file.length()));
+
+            channel.read(buffer);
+
+            buffer.flip();
+
+            while (buffer.hasRemaining()) {
+                System.out.print((char) buffer.get());
+            }
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
