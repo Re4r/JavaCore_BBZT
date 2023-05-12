c6e8d62c4584d048839d9d8f6569c4fe71312551
KirillMar <re4r@bk.ru>
2023-05-12 12:58:01 +0300

    io/nio

--- src/streams/Exec915.java
+++ src/streams/Exec915.java
@@ -4,18 +4,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Exec915 {
     public static void main(String[] args) throws IOException {
 
         Path filePath = Paths.get("test15.txt");
+        Path filePath2 = Paths.get("D:\\FileTest\\MFL\\text16.txt");
         Path directoryPath = Paths.get("D:\\FileTest\\MFL");
+        Path directoryPath2 = Paths.get("D:\\FileTest\\C");
 
         System.out.println("--------------------------------");
 
 //        Files.createDirectory(directoryPath);
 
-        Files.copy(filePath, directoryPath.resolve(filePath));
+//        Files.copy(filePath, directoryPath.resolve(filePath), StandardCopyOption.REPLACE_EXISTING);
+//        Files.copy(filePath, directoryPath.resolve("text16.txt"), StandardCopyOption.REPLACE_EXISTING);
+
+        System.out.println("--------------------------------");
+
+//        Files.copy(directoryPath2, directoryPath.resolve("B"));
+//        Files.move(filePath, directoryPath2.resolve("test15.txt"));
+        Files.move(filePath2, directoryPath2.resolve("test17.txt"));
+
 
     }
 }
