5a8142c0562a2077fcc22c3987b0f20c9ca3bf74
KirillMar <re4r@bk.ru>
2023-05-12 12:40:31 +0300

    io/nio

--- src/streams/Exec915.java
+++ src/streams/Exec915.java
@@ -1,4 +1,21 @@
 package streams;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Exec915 {
+    public static void main(String[] args) throws IOException {
+
+        Path filePath = Paths.get("test15.txt");
+        Path directoryPath = Paths.get("D:\\FileTest\\MFL");
+
+        System.out.println("--------------------------------");
+
+//        Files.createDirectory(directoryPath);
+
+        Files.copy(filePath, directoryPath.resolve(filePath));
+
+    }
 }
