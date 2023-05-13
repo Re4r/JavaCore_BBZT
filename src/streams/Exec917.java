c06e59b47b2abfb97eea848b64f979d0b4960917
KirillMar <re4r@bk.ru>
2023-05-13 22:46:25 +0300

    io/nio

--- src/streams/Exec917.java
+++ src/streams/Exec917.java
@@ -1,14 +1,16 @@
 package streams;
 
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
+import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class Exec917 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Path source = Paths.get("D:\\FileTest\\MFL");
+        Path destination = Paths.get("D:\\FileTest\\DEST");
+
+        Files.walkFileTree(source, new MyFileVisitor(source, destination));
 
     }
 }
@@ -25,14 +27,14 @@ class MyFileVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
         Path newDestination = destination.resolve(source.relativize(dir));
-        Files.copy(dir, newDestination);
+        Files.copy(dir, newDestination, StandardCopyOption.REPLACE_EXISTING);
         return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         Path newDestination = destination.resolve(source.relativize(file));
-        Files.copy(file, newDestination);
+        Files.copy(file, newDestination, StandardCopyOption.REPLACE_EXISTING);
         return FileVisitResult.CONTINUE;
     }
 }
