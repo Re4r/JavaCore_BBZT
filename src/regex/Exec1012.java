1a5e0c6efa01ad28cd1c0e621b1650f44e13f066
KirillMar <re4r@bk.ru>
2023-05-13 23:13:13 +0300

    io/nio

--- src/regex/Exec1012.java
+++ src/regex/Exec1012.java
@@ -1,7 +1,18 @@
 package regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Exec1012 {
     public static void main(String[] args) {
 
+        String s = "ABCDABCFABCGABCRABCDABCD";
+
+        Pattern pattern = Pattern.compile("ABC");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
     }
 }
