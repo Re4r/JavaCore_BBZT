cd446a5c249cb7aea235e14eb56d47b27752216c
KirillMar <re4r@bk.ru>
2023-05-13 22:07:11 +0300

    io/nio

--- src/streams/Exec916.java
+++ src/streams/Exec916.java
@@ -1,9 +1,8 @@
 package streams;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Exec916 {
     public static void main(String[] args) throws IOException {
@@ -13,6 +12,34 @@ public class Exec916 {
 //        Files.createDirectory(Path.of("D:\\FileTest\\MFL\\V"));
 //        Files.createDirectory(Path.of("D:\\FileTest\\MFL\\K"));
 //        Files.createDirectory(Path.of("D:\\FileTest\\MFL\\RT"));
+
+        FileVisitor<Path> pathFileVisitor = new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("Enter to directory " + dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println("File name: " + file.getFileName());
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                System.out.println("Error while visiting file: " + file.getFileName());
+                return FileVisitResult.TERMINATE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                System.out.println("Exit from directory: " + dir);
+                return FileVisitResult.CONTINUE;
+            }
+        };
+
+        Files.walkFileTree(path, pathFileVisitor);
 
 
     }
