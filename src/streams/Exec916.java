2ce92993d0053afbe7ad76eae567bc3316e0ac18
KirillMar <re4r@bk.ru>
2023-05-13 22:08:31 +0300

    io/nio

--- src/streams/Exec916.java
+++ src/streams/Exec916.java
@@ -16,25 +16,25 @@ public class Exec916 {
         FileVisitor<Path> pathFileVisitor = new FileVisitor<Path>() {
             @Override
             public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                System.out.println("Enter to directory " + dir);
+                System.out.println("Enter to directory >>> " + dir);
                 return FileVisitResult.CONTINUE;
             }
 
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                System.out.println("File name: " + file.getFileName());
+                System.out.println("File name >>> " + file.getFileName());
                 return FileVisitResult.CONTINUE;
             }
 
             @Override
             public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                System.out.println("Error while visiting file: " + file.getFileName());
+                System.out.println("Error while visiting file >>> " + file.getFileName());
                 return FileVisitResult.TERMINATE;
             }
 
             @Override
             public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                System.out.println("Exit from directory: " + dir);
+                System.out.println("Exit from directory >>> " + dir);
                 return FileVisitResult.CONTINUE;
             }
         };
