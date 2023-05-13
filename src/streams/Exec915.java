18150fe69b45be8ac6cf371dea8ff5731f82b554
KirillMar <re4r@bk.ru>
2023-05-13 21:37:15 +0300

    io/nio

--- src/streams/Exec915.java
+++ src/streams/Exec915.java
@@ -9,19 +9,19 @@ import java.nio.file.StandardCopyOption;
 public class Exec915 {
     public static void main(String[] args) throws IOException {
 
-        Path filePath = Paths.get("test15.txt");
-        Path filePath2 = Paths.get("D:\\FileTest\\MFL\\text16.txt");
-        Path directoryPath = Paths.get("D:\\FileTest\\MFL");
-        Path directoryPath2 = Paths.get("D:\\FileTest\\C");
+//        Path filePath = Paths.get("test15.txt");
+//        Path filePath2 = Paths.get("D:\\FileTest\\MFL\\text16.txt");
+//        Path directoryPath = Paths.get("D:\\FileTest\\MFL");
+//        Path directoryPath2 = Paths.get("D:\\FileTest\\C");
 
-        System.out.println("--------------------------------");
+//        System.out.println("--------------------------------");
 
 //        Files.createDirectory(directoryPath);
 
 //        Files.copy(filePath, directoryPath.resolve(filePath), StandardCopyOption.REPLACE_EXISTING);
 //        Files.copy(filePath, directoryPath.resolve("text16.txt"), StandardCopyOption.REPLACE_EXISTING);
 
-        System.out.println("--------------------------------");
+//        System.out.println("--------------------------------");
 
 //        Files.copy(directoryPath2, directoryPath.resolve("B"));
 //        Files.move(filePath, directoryPath2.resolve("test15.txt"));
@@ -29,6 +29,11 @@ public class Exec915 {
 
 //        Files.delete(Paths.get("D:\\FileTest\\MFL\\test15.txt"));
 //        Files.deleteIfExists(Paths.get("D:\\FileTest\\MFL\\B"));
+
+        Path filePath = Paths.get("test10.txt");
+        Files.createFile(filePath);
+        String s = "AQAQAQAQAQAQA";
+        Files.write(filePath, s.getBytes());
 
 
     }
