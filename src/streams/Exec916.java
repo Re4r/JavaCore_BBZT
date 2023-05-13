5613946d6c1bb39dc5fdc81c029a003fe94881bc
KirillMar <re4r@bk.ru>
2023-05-13 21:56:14 +0300

    io/nio

--- src/streams/Exec916.java
+++ src/streams/Exec916.java
@@ -1,4 +1,19 @@
 package streams;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Exec916 {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("D:\\FileTest\\MFL");
+
+//        Files.createDirectory(Path.of("D:\\FileTest\\MFL\\V"));
+//        Files.createDirectory(Path.of("D:\\FileTest\\MFL\\K"));
+//        Files.createDirectory(Path.of("D:\\FileTest\\MFL\\RT"));
+
+
+    }
 }
