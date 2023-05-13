c524e6ab008b42acfcd68d345cc9c4b1bc9819af
KirillMar <re4r@bk.ru>
2023-05-13 23:30:42 +0300

    io/nio

--- src/regex/Exec1012.java
+++ src/regex/Exec1012.java
@@ -6,7 +6,8 @@ import java.util.regex.Pattern;
 public class Exec1012 {
     public static void main(String[] args) {
 
-        String s = "ABCDABCFA4357B1CGABCRABCDABCD83484JAERTY89054321QWERTYZVXBC";
+        String s = "ABCDABCFA4357B1CGABCRABCDABCD83484JAERTY89054321QWERTYZVXBC"
+                + "AJKDSBDHBH435GHJBJHVDGVSG0004HJGYUGYUVJHDVH465877HFBCV32872";
 
         Pattern pattern = Pattern.compile("ABC[D|F]");
         Matcher matcher = pattern.matcher(s);
