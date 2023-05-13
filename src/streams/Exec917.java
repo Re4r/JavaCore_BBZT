616c0164ab7f7ff50b81fabc1af3d21a3b59dd85
KirillMar <re4r@bk.ru>
2023-05-13 22:19:49 +0300

    io/nio

--- src/streams/Exec917.java
+++ src/streams/Exec917.java
@@ -1,7 +1,34 @@
 package streams;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class Exec917 {
     public static void main(String[] args) {
 
+    }
+}
+
+class MyFileVisitor extends SimpleFileVisitor<Path> {
+    private Path source;
+    private Path destination;
+
+    public MyFileVisitor(Path source, Path destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(dir));
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        return super.visitFile(file, attrs);
     }
 }
