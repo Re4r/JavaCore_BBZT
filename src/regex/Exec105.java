936ca1c7b28673750f878540b6ce6f6801062b51
KirillMar <re4r@bk.ru>
2023-05-15 21:25:13 +0300

    io/nio

--- src/regex/Exec105.java
+++ src/regex/Exec105.java
@@ -3,8 +3,8 @@ package regex;
 public class Exec105 {
     public static void main(String[] args) {
 
-        String s1 = "AAZAAZAZAZAZAZAZAZAZAB";
-        String s2 = s1.replace("ZAB", "SQL");
+        String s1 = "AAZAAZAZAZAZAZAZAZAZABAX";
+        String s2 = s1.replace("ZABAX", "SQL");
         System.out.println(s2);
         String s3 = s2.replaceAll("A{2}", "PEEP");
         System.out.println(s3);
