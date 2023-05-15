a901c027abac351aad6d85dc4e906919f4f88920
KirillMar <re4r@bk.ru>
2023-05-15 20:45:42 +0300

    io/nio

--- src/regex/Exec104.java
+++ src/regex/Exec104.java
@@ -1,5 +1,7 @@
 package regex;
 
+import java.util.Arrays;
+
 public class Exec104 {
     public static void main(String[] args) {
 
@@ -10,6 +12,12 @@ public class Exec104 {
                 "email: masha@mail.ru, Postcode: UKR54, Phone Number: +987654321;"
                 +"Chuck Norris, USA, Hollywood, All stars street, 87, Flat 21," +
                 "email: chuck@gmail.com, Postcode: USA23, Phone Number: +136478952.";
+
+        String s2 = "chuck@gmail.com";
+
+        System.out.println(s2.matches("\\w+@\\w+\\.(ru|com)"));
+        String [] strings = s.split(" ");
+        System.out.println(Arrays.toString(strings));
 
     }
 }
