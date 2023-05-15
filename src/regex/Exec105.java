f701ca7ede0474eb8ab22f896b0a2b8ce762b776
KirillMar <re4r@bk.ru>
2023-05-15 20:56:42 +0300

    io/nio

--- src/regex/Exec105.java
+++ src/regex/Exec105.java
@@ -3,5 +3,10 @@ package regex;
 public class Exec105 {
     public static void main(String[] args) {
 
+        String s1 = "AAZAZAZAZAZAZAZAZAZAB";
+        String s2 = s1.replace("ZAB", "SQL");
+
+        System.out.println(s2);
+
     }
 }
