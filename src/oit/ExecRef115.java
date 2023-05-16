4f14b361055e8eac3cde45cb4e97da531da9c797
KirillMar <re4r@bk.ru>
2023-05-16 20:59:08 +0300

    reflection

--- src/oit/ExecRef115.java
+++ src/oit/ExecRef115.java
@@ -1,19 +1,29 @@
 package oit;
 
+import java.lang.reflect.Field;
+
 public class ExecRef115 {
     public static void main(String[] args) {
 
+//        try {
+//            Class employeeClass = Class.forName("oit.Employee");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+
+//        Employee employee = new Employee();
+//
+//        Class employeeClass3 = employee.getClass();
+
+        Class<Employee> employeeClass = Employee.class;
+
         try {
-            Class employeeClass = Class.forName("oit.Employee");
-        } catch (ClassNotFoundException e) {
+            Field fieldId = employeeClass.getField("id");
+            System.out.println("Type of filed id: " + fieldId.getType());
+        } catch (NoSuchFieldException e) {
             throw new RuntimeException(e);
         }
 
-        Class<Employee> employeeClass2 = Employee.class;
-
-        Employee employee = new Employee();
-
-        Class employeeClass3 = employee.getClass();
 
     }
 }
