3f7b326ccdc81e7d295239cb382475dfeb820033
KirillMar <re4r@bk.ru>
2023-05-16 20:01:49 +0300

    oit

--- src/oit/Exec113.java
+++ src/oit/Exec113.java
@@ -1,7 +1,14 @@
 package oit;
 
+import java.util.Scanner;
+
 public class Exec113 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter digit: ");
+        int i = scanner.nextInt();
+        System.out.println("Digit: " + i);
 
     }
 }
