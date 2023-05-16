a548744f50549d3e555f5b367eb99b8d38339ffe
KirillMar <re4r@bk.ru>
2023-05-16 20:54:40 +0300

    reflection

--- src/oit/ExecRef115.java
+++ src/oit/ExecRef115.java
@@ -3,5 +3,17 @@ package oit;
 public class ExecRef115 {
     public static void main(String[] args) {
 
+        try {
+            Class employeeClass = Class.forName("oit.Employee");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        Class<Employee> employeeClass2 = Employee.class;
+
+        Employee employee = new Employee();
+
+        Class employeeClass3 = employee.getClass();
+
     }
 }
