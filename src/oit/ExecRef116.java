9695661db8134c20c2bff9acdcff975650033925
KirillMar <re4r@bk.ru>
2023-05-17 19:21:37 +0300

    reflection

--- src/oit/ExecRef116.java
+++ src/oit/ExecRef116.java
@@ -2,6 +2,7 @@ package oit;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ExecRef116 {
 
@@ -12,8 +13,21 @@ public class ExecRef116 {
         try {
             Constructor<Employee> constructor = employeeClass.getConstructor();
             Constructor<Employee> constructor1 = employeeClass.getConstructor(int.class, String.class, String.class);
+
             Employee employee = constructor.newInstance();
-            Employee employee1 = constructor1.newInstance();
+            Employee employee1 = constructor1.newInstance(5, "Averty", "IT");
+
+            System.out.println(employee1);
+
+            Method method = employeeClass.getMethod("setSalary", double.class);
+
+            method.invoke(employee1, 1800);
+
+            System.out.println(employee1);
+
+
+
+
         } catch (NoSuchMethodException e) {
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
