01ba7d71e29144ffa182228fb9b2a89261705d95
KirillMar <re4r@bk.ru>
2023-05-17 19:34:10 +0300

    reflection

--- src/oit/ExecRef1162.java
+++ src/oit/ExecRef1162.java
@@ -1,7 +1,29 @@
 package oit;
 
+import java.lang.reflect.Field;
+
 public class ExecRef1162 {
     public static void main(String[] args) {
+
+        Employee employee = new Employee(17, "Gertyup", "IT");
+
+        Class<Employee> employeeClass = Employee.class;
+
+        try {
+            Field field = employeeClass.getDeclaredField("salary");
+
+            field.setAccessible(true);
+
+            double salaryValue = field.getDouble(employee);
+
+            System.out.println(salaryValue);
+
+
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
