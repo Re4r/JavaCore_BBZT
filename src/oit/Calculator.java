f0b9f056b56c8f5d3175b263e0d29da853ff5adc
KirillMar <re4r@bk.ru>
2023-05-17 19:45:40 +0300

    reflection

--- src/oit/Calculator.java
+++ src/oit/Calculator.java
@@ -1,5 +1,10 @@
 package oit;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Calculator {
 
     void calc(int x, int y) {
@@ -7,5 +12,26 @@ public class Calculator {
         System.out.println(" - " + (x - y));
         System.out.println(" * " + (x * y));
         System.out.println(" / " + (x / y));
+    }
+}
+
+class TestCalculator {
+    public static void main(String[] args) {
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("dest.txt"))) {
+
+            String methodName = bufferedReader.readLine();
+            String firstArg = bufferedReader.readLine();
+            String secondArg = bufferedReader.readLine();
+
+            Calculator calculator = new Calculator();
+
+            Class<Calculator> calculatorClass = Calculator.class;
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
