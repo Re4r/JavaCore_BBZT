f44537730417a01807434c1272e81a0f656f46a8
KirillMar <re4r@bk.ru>
2023-05-17 19:05:07 +0300

    reflection

--- src/oit/ExecRef116.java
+++ src/oit/ExecRef116.java
@@ -1,8 +1,28 @@
 package oit;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class ExecRef116 {
 
     Class<Employee> employeeClass = Employee.class;
+
+    Constructor<Employee> employeeConstructor;
+
+    {
+        try {
+            employeeConstructor = employeeClass.getConstructor();
+            Employee employee = employeeConstructor.newInstance();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
