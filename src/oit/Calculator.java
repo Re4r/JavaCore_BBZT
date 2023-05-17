bec569ac0709cfa0d2022d466f070eebe2d01064
KirillMar <re4r@bk.ru>
2023-05-17 19:50:40 +0300

    reflection

--- src/oit/Calculator.java
+++ src/oit/Calculator.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class Calculator {
 
@@ -28,9 +31,27 @@ class TestCalculator {
 
             Class<Calculator> calculatorClass = Calculator.class;
 
+            Method method = null;
+
+            Method[] methods = calculatorClass.getDeclaredMethods();
+            System.out.println(Arrays.toString(methods));
+
+            for (Method myMethod : methods) {
+                if (myMethod.getName().equals(methodName)) {
+                    method = myMethod;
+                }
+            }
+
+            method.invoke(calculator, Integer.parseInt(firstArg), Integer.parseInt(secondArg));
+
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
