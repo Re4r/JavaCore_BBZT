56f4a33b56d4f6f9c2567fa8372f33ae85d7d6d2
KirillMar <re4r@bk.ru>
2023-05-17 20:06:11 +0300

    annotations

--- src/annotations/Exec117.java
+++ src/annotations/Exec117.java
@@ -2,3 +2,25 @@ package annotations;
 
 public class Exec117 {
 }
+
+class EmployeeYZ {
+    private String name;
+    private double salary;
+
+    public EmployeeYZ(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void increaseSalary() {
+        salary *= 2;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeYZ{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
